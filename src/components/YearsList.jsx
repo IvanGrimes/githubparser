31a32fe11abf9399d409c7ceb5c2005c0a4403e7
Ivan Grimes <ivan@grimesco.de>
2018-08-31 17:27:20 +0300

    Complete YearsList

--- src/components/YearsList.jsx
+++ src/components/YearsList.jsx
@@ -1,16 +1,51 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import YearItem from './YearItem';
 
 export default class YearsList extends Component {
+  static propTypes = {
+    repositories: PropTypes.arrayOf(PropTypes.any).isRequired,
+  };
+
+  get uniqueYears() { // TODO: Refactor it!
+    const { repositories } = this.props;
+    let years = new Set();
+
+    repositories.forEach((repository) => {
+      years.add(new Date(repository.created_at).getFullYear());
+    });
+
+    years = Array.from(years).sort((a, b) => a > b);
+
+    return years;
+  }
+
+  handleClick = (ev) => {
+    const { textContent } = ev.target;
+    ev.preventDefault();
+
+    console.log(+textContent);
+  }
+
+  renderItems() {
+    const items = this.uniqueYears.map((year, index) => {
+      return (
+        <button
+          type="button"
+          key={+(new Date().getTime() / (index + 1)).toFixed()}
+          onClick={this.handleClick}
+        >
+          {year}
+        </button>
+      );
+    });
+
+    return items;
+  }
+
   render() {
     return (
       <Fragment>
-        <YearItem />
-        <YearItem />
-        <YearItem />
-        <YearItem />
-        <YearItem />
+        {this.renderItems()}
       </Fragment>
     );
   }
--- src/containers/YearsListContainer.jsx
+++ src/containers/YearsListContainer.jsx
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import YearsList from '../components/YearsList';
 
-export default class YearsListContainer extends Component {
+const mapStateToProps = store => ({
+  repositories: store.account.repositories,
+});
+
+class YearsListContainer extends Component {
+  static propTypes = {
+    repositories: PropTypes.arrayOf(PropTypes.any).isRequired,
+  };
+
   render() {
+    const { repositories } = this.props;
+
     return (
       <div>
-        <YearsList />
+        <YearsList
+          repositories={repositories}
+        />
       </div>
     );
   }
 }
+
+export default connect(mapStateToProps)(YearsListContainer);
