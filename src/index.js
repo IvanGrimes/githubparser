49caf58d9d937ae62ad231b23b9e0a7f708a6dcb
Ivan Grimes <ivan@grimesco.de>
2018-09-04 18:24:40 +0300

    remove store from window

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux';
 import store from './store/configureStore';
 
 ReactDOM.render(
